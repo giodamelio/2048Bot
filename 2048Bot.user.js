b01ec5201d61687839534ab9840866e454777ea0
Gio d'Amelio <giodamelio@gmail.com>
2014-04-13 21:08:58 -0700

    Add tick counter

--- 2048Bot.user.js
+++ 2048Bot.user.js
@@ -10,9 +10,11 @@
 // ==/UserScript==
 
 // Add our button
-$(".above-game").after("<div class=\"above-game\"><a class=\"restart-button automate-button\">Auto Solve</a></div>");
+$(".above-game").after("<div class=\"above-game\"><a class=\"restart-button automate-button\">Auto Solve</a><p class=\"game-intro tick-display\">Tick 0</p></div>");
 $(".automate-button").css("float", "left");
 $(".automate-button").css("margin-top", "5px");
+$(".tick-display").css("margin-left", "8px");
+$(".tick-display").css("margin-top", "5px");
 $(".game-container").css("margin-top", "20px");
 
 var move = function(direction) {
@@ -25,8 +27,14 @@ var randomIntFromInterval = function randomIntFromInterval(min, max) {
 }
 
 $(".automate-button").click(function() {
+    var tick = 1;
     var directions = ["right", "down", "up", "left"];
     setInterval(function() {
+        // Move the tiles
         move(directions[randomIntFromInterval(0, 3)]);
+
+        // Update the tick
+        $(".tick-display").text("Tick " + tick);
+        tick = tick + 1;
     }, 500);
 });
