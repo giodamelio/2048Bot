be698980a9767f576120f101c6fabd0a5b09c07a
Gio d'Amelio <giodamelio@gmail.com>
2014-04-13 21:09:21 -0700

    Rearange metadata

--- 2048Bot.user.js
+++ 2048Bot.user.js
@@ -3,9 +3,11 @@
 // @namespace       http://use.i.E.your.homepage/
 // @version         0.1.0
 // @description     Too lazy to get your own 2048, let this bot take care of it for you.
+// @copyright       2012+, Gio d'Amelio
+//
+// @grant           none
 // @match           http://gabrielecirulli.github.io/2048/
 // @match           http://localhost:3141/
-// @copyright       2012+, Gio d"Amelio
 // @require         http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // ==/UserScript==
 
